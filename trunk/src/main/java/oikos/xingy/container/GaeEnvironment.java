b4a14fc7397407c3da8bf479f4d5dabbab711c0e
leandro.saad@gmail.com <leandro.saad@gmail.com@89160b08-a2a5-b66a-904c-244e75345a17>
2010-08-14 20:18:50 +0000

    deploying to Google App Engine

---
+++ trunk/src/main/java/oikos/xingy/container/GaeEnvironment.java
@@ -0,0 +1,19 @@
+package oikos.xingy.container;
+
+import org.apache.commons.lang.SystemUtils;
+
+import br.com.ibnetwork.xingu.container.impl.EnvironmentImpl;
+
+public class GaeEnvironment
+    extends EnvironmentImpl
+{
+    @Override
+    public void initialize() 
+        throws Exception
+    {
+        String userDir = SystemUtils.getUserDir().getAbsolutePath();
+        put("user.dir", userDir);
+        put("${app.root}", userDir);
+        put("${pom.build.testOutputDirectory}", userDir);
+    }
+}
